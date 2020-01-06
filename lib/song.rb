4ecc8fdd70d4115112ad734178cbca40ec040ae1
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-06 21:18:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,10 @@ class Song
   
   def self.find_or_create_by_name(name)
     if self.find_by_name == nil 
-      
+      self.create_by_name
+    else 
+      self.find_by_name
+    end 
   end 
     
 
